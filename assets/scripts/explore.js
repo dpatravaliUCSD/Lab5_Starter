e4ebe52d49c849fe335b63b74ecb09d2ba9d8526
dhruv-patravali <dpatravali2022@francisparker.org>
2024-05-07 23:00:39 -0700

    lab 5 part 1 and part2/1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,53 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.querySelector("select");
+  const inputTxt = document.getElementById('text-to-speak');
+  const talk = document.querySelector('button');
+
+  const imgHappy = document.querySelector('#explore img'); 
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  voiceSelect.addEventListener('click', function(){
+    populateVoiceList();
+  });
+
+  talk.addEventListener('click', function(){
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+    
+    var talkingFace = "assets/images/smiling-open.png";
+    var silentFace = "assets/images/smiling.png";
+    utterThis.onstart = function() {
+      imgHappy.src = talkingFace;
+    }
+    utterThis.onend = function() {
+      imgHappy.src = silentFace;
+    }
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,70 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const jsConfetti = new JSConfetti();
+
 function init() {
-  // TODO
+  const chooseHorn = document.getElementById('horn-select');
+  const hornPic = document.querySelector('#expose img');
+  const playSound = document.querySelector('#expose button');
+  const audio = document.querySelector('#expose audio');
+  const volumeInput = document.getElementById('volume');
+  const volumeLogo = document.querySelector('#volume-controls img');
+
+  //choose which horn
+  chooseHorn.addEventListener('change', function() {
+    if (this.value === 'air-horn') {
+      hornPic.src = 'assets/images/air-horn.svg';
+      hornPic.alt = 'Air Horn';
+      audio.src = 'assets/audio/air-horn.mp3';
+    } 
+    else if (this.value === 'car-horn') {
+      hornPic.src = 'assets/images/car-horn.svg';
+      hornPic.alt = 'No image selected';
+      audio.src = 'assets/audio/car-horn.mp3';
+    } 
+    else if (this.value === 'party-horn') {
+      hornPic.src = 'assets/images/party-horn.svg';
+      hornPic.alt = 'No image selected';
+      audio.src = 'assets/audio/party-horn.mp3';
+    }
+  });
+
+  //play horn sounds
+  playSound.addEventListener('click', function() {
+    if (chooseHorn.value === 'air-horn') {
+      audio.play();
+    }
+    else if (chooseHorn.value === 'car-horn') {
+      audio.play();
+    }
+    else if (chooseHorn.value === 'party-horn') {
+      audio.play();
+      jsConfetti.addConfetti();
+    }
+  });
+
+  //change volume
+  volumeInput.addEventListener('input', function() {
+    let volumeValue = parseInt(this.value, 10);
+    audio.volume = volumeValue / 100;
+
+    if (volumeValue >= 1 && volumeValue < 33) {
+      volumeLogo.src = 'assets/icons/volume-level-1.svg';
+      volumeLogo.alt = 'Volume level: 1';
+    } 
+    else if (volumeValue >= 33 && volumeValue < 67) {
+      volumeLogo.src = 'assets/icons/volume-level-2.svg';
+      volumeLogo.alt = 'Volume level: 2';
+    } 
+    else if (volumeValue >= 67) {
+      volumeLogo.src = 'assets/icons/volume-level-3.svg';
+      volumeLogo.alt = 'Volume level: 3';
+    } 
+    else { //volume 0
+      volumeLogo.src = 'assets/icons/volume-level-0.svg';
+      volumeLogo.alt = 'Volume level: 0';
+    }
+});
+
 }
